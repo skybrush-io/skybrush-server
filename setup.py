fb525ffd492d46574c6ec5cb343011b9b30ca726
Tamas Nepusz <ntamas@gmail.com>
2016-01-11 22:43:32 +0100

    trying to get the dependencies right for Heroku

--- setup.py
+++ setup.py
@@ -2,7 +2,16 @@
 
 from setuptools import setup, find_packages
 
-requires = [line.strip() for line in open("requirements.txt")]
+requires = [
+    "bitstring>=3.1.3",
+    "click>=6.2",
+    "eventlet>=0.17.4",
+    "Flask>=0.10.1",
+    "flask-socketio>=1.0",
+    "jsonschema>=2.5.1",
+    "pyserial>=2.7",
+    "python-socketio>=0.6.1"
+]
 
 __version__ = None
 exec(open("flockwave/server/version.py").read())

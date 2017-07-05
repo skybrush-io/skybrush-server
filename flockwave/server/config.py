d1bb73b7261ca9d00cc88342add19a9faa59fddd
Tamas Nepusz <ntamas@gmail.com>
2017-07-05 10:51:20 +0200

    moved fake UAV home to another location to ease testing for 360world

--- flockwave/server/config.py
+++ flockwave/server/config.py
@@ -26,6 +26,12 @@ EXTENSIONS = {
         "delay": 0.04 if IN_HEROKU else 1.9,
         "id_format": "FAKE-{0:02}",
         "center": {
+            # 360world.eu teto
+            "lat": 47.483717,
+            "lon": 19.015107,
+            "alt": 20
+        } if IN_HEROKU else {
+            # ELTE kert
             "lat": 47.473360,
             "lon": 19.062159,
             "alt": 20

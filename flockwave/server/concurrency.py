a091471475303ade0cb706435069e1aa07686e2d
Tamas Nepusz <ntamas@gmail.com>
2019-09-28 12:41:41 +0200

    added empty slugignore file as Heroku did not seem to recognize the removal

--- flockwave/server/concurrency.py
+++ flockwave/server/concurrency.py
@@ -1,8 +1,10 @@
 """Concurrency-related utility functions."""
 
 from functools import wraps
+from trio_util import MailboxRepeatedEvent
+from typing import Any, Iterable, Set
 
-__all__ = ("cancellable",)
+__all__ = ("AsyncSet", "cancellable")
 
 
 def cancellable(func):
@@ -18,3 +20,51 @@ def cancellable(func):
     decorated._cancellable = True
 
     return decorated
+
+
+class AsyncCollectorSet:
+    """Asynchronous set object that supports the following operations:
+
+    - Adding one or more items to the set
+
+    - Waiting for the set to become non-empty and then removing all items
+      from the set in one operation.
+
+    This object is typically used in a producer-consumer setting. Producers
+    add items to the set either one by one (with `add()`) or in batches
+    (with `update()`). At the same time, a single consumer iterates over the
+    set asynchronously and takes all items from it in each iteration.
+    """
+
+    def __init__(self):
+        """Constructor."""
+        self._data = set()
+        self._event = MailboxRepeatedEvent()
+
+    def add(self, item: Any) -> None:
+        """Adds a single item to the set.
+
+        Parameters:
+            item: the item to add
+        """
+        self._data.add(item)
+        self._event.set()
+
+    def update(self, items: Iterable[Any]):
+        """Updates the set with multiple items from an iterable.
+
+        Parameters:
+            items: the items to add
+        """
+        self._data.update(items)
+        if self._data:
+            self._event.set()
+
+    async def __aiter__(self):
+        """Asynchronously iterates over non-empty batches of items that
+        were added to the set.
+        """
+        async for _ in self._event:
+            result = set(self._data)
+            self._data.clear()
+            yield result

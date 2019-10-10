265605618bdd66c6326ce9723b3e250048298a3f
Tamas Nepusz <ntamas@gmail.com>
2019-10-10 18:27:08 +0200

    added EncoderChannel and MessageChannel

--- src/flockwave/channels.py
+++ src/flockwave/channels.py
@@ -5,11 +5,10 @@ ReadableConnection_ and yields parsed message objects.
 from collections import deque
 from inspect import iscoroutinefunction
 from trio import EndOfChannel
-from trio.abc import ReceiveChannel
-from typing import Awaitable, Callable, Generic, TypeVar, Union
+from trio.abc import Channel, ReceiveChannel, SendChannel
+from typing import Awaitable, Callable, List, TypeVar, Union
 
-from .connections import ReadableConnection, WritableConnection
-from .parsers import Parser
+from .connections import Connection, ReadableConnection, WritableConnection
 
 __all__ = ("ParserChannel",)
 
@@ -18,8 +17,11 @@ T = TypeVar("T")
 Reader = Union[Callable[[], Awaitable[bytes]], ReadableConnection[bytes]]
 Writer = Union[Callable[[bytes], None], WritableConnection[bytes]]
 
+Parser = Callable[[bytes], List[T]]
+Encoder = Callable[[T], bytes]
 
-class ParserChannel(ReceiveChannel[T], Generic[T]):
+
+class ParserChannel(ReceiveChannel[T]):
     """Trio-style ReceiveChannel_ that takes data from a ReadableConnection_
     and yields parsed message objects.
     """
@@ -37,7 +39,13 @@ class ParserChannel(ReceiveChannel[T], Generic[T]):
                 f"ReadableConnection or async function expected, got {type(reader)}"
             )
 
-        self._feeder = parser.feed
+        if callable(getattr(parser, "feed", None)):
+            self._parser = parser.feed
+        elif callable(parser):
+            self._parser = parser
+        else:
+            raise TypeError(f"Parser or callable expected, got {type(parser)}")
+
         self._pending = deque()
 
     async def aclose(self) -> None:
@@ -47,15 +55,86 @@ class ParserChannel(ReceiveChannel[T], Generic[T]):
     async def receive(self) -> T:
         while not self._pending:
             await self._read()
-
         return self._pending.popleft()
 
     async def _read(self) -> None:
         """Reads the pending bytes using the associated reader function and
         feeds the parsed messages into the pending list.
+
+        Raises:
+            EndOfChannel: if there is no more data to read from the connection
         """
         data = await self._reader()
         if not data:
             raise EndOfChannel()
+        self._pending.extend(self._parser(data))
 
-        self._pending.extend(self._feeder(data))
+
+class EncoderChannel(SendChannel[T]):
+    """Trio-style SendChannel_ that encodes objects and writes them to a
+    WritableConnection_.
+    """
+
+    def __init__(self, writer: Writer, encoder: Encoder[T]):
+        if iscoroutinefunction(getattr(writer, "write", None)):
+            # Reader is a Connection
+            self._writer = writer.writer
+            self._closer = getattr(writer, "aclose", None)
+        elif iscoroutinefunction(writer):
+            self._writer = writer
+            self._closer = None
+        else:
+            raise TypeError(
+                f"WritableConnection or async function expected, got {type(writer)}"
+            )
+
+        self._encoder = encoder
+
+    async def aclose(self) -> None:
+        if self._closer:
+            await self._closer()
+
+    async def send(self, value: T) -> None:
+        await self._writer(self._encoder(value))
+
+
+class MessageChannel(Channel[T]):
+    """Trio-style Channel_ that wraps a readable-writable connection and
+    uses a parser to decode the messages read from the connection and an
+    encoder to encode the messages to the wire format of the connection.
+    """
+
+    def __init__(self, connection: Connection, parser: Parser[T], encoder: Encoder[T]):
+        self._connection = connection
+        self._encoder = encoder
+        self._pending = deque()
+
+        if callable(getattr(parser, "feed", None)):
+            self._parser = parser.feed
+        elif callable(parser):
+            self._parser = parser
+        else:
+            raise TypeError(f"Parser or callable expected, got {type(parser)}")
+
+    async def aclose(self) -> None:
+        await self._connection.close()
+
+    async def receive(self) -> T:
+        while not self._pending:
+            await self._read()
+        return self._pending.popleft()
+
+    async def send(self, value: T) -> None:
+        await self._connection.write(self._encoder(value))
+
+    async def _read(self) -> None:
+        """Reads the pending bytes using the associated reader function and
+        feeds the parsed messages into the pending list.
+
+        Raises:
+            EndOfChannel: if there is no more data to read from the connection
+        """
+        data = await self._connection.read()
+        if not data:
+            raise EndOfChannel()
+        self._pending.extend(self._parser(data))

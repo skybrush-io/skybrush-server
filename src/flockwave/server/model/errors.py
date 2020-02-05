317638aa4de75391e4dfd75df88470b00791b56c
Tamas Nepusz <ntamas@gmail.com>
2020-02-05 10:31:00 +0100

    added error codes and descriptions

--- src/flockwave/server/model/errors.py
+++ src/flockwave/server/model/errors.py
@@ -47,6 +47,107 @@ class NoSuchPathError(FlockwaveError):
         self.path = path
 
 
+#: Dictionary mapping Flockwave error codes to more-or-less human-readable
+#: abbreviations
+_error_code_to_abbreviation = {
+    0: "ok",
+    1: "disarm",
+    2: "no log",
+    3: "prearm",
+    4: "init",
+    5: "takeoff",
+    6: "landing",
+    7: "landed",
+    64: "storage",
+    65: "RC lost",
+    66: "lowbat",
+    67: "timesync",
+    128: "comm t/o",
+    129: "ack t/o",
+    130: "proto",
+    131: "prearm",
+    132: "RC lost",
+    133: "no GPS",
+    134: "lowbat",
+    135: "target",
+    136: "too far",
+    137: "config",
+    188: "simerr",
+    189: "control",
+    190: "sensor",
+    191: "error",
+    192: "compat",
+    193: "mag",
+    194: "gyro",
+    195: "acc",
+    196: "baro",
+    197: "GPS",
+    198: "motor",
+    199: "lowbat",
+    200: "home",
+    201: "fence",
+    202: "clk",
+    203: "extclk",
+    204: "no HW",
+    205: "initfail",
+    206: "commfail",
+    207: "crash",
+    253: "simcrit",
+    254: "sensor",
+    255: "fatal",
+}
+
+
+#: Dictionary mapping Flockwave error codes to human-readable descriptions.
+_error_code_to_description = {
+    0: "No error",
+    1: "Drone not armed yet",
+    2: "Logging deactivated",
+    3: "Prearm check in progress",
+    4: "Autopilot initializing",
+    5: "Drone is taking off",
+    6: "Drone is landing",
+    7: "Drone has landed successfully",
+    64: "Low disk space",
+    65: "RC lost",
+    66: "Battery low",
+    67: "Timesync error",
+    128: "Autopilot communication timeout",
+    129: "Autopilot acknowledgment timeout",
+    130: "Autopilot communication protocol error",
+    131: "Prearm check failure",
+    132: "RC signal lost",
+    133: "GPS signal lost or GPS error",
+    134: "Battery low",
+    135: "Target not found",
+    136: "Target is too far",
+    137: "Configuration error",
+    188: "Simulated error",
+    189: "Error in control algorithm",
+    190: "Unspecified sensor failure",
+    191: "Unspecified error",
+    192: "Incompatible hardware or software",
+    193: "Magnetometer error",
+    194: "Gyroscope error",
+    195: "Accelerometer error",
+    196: "Pressure sensor or altimeter error",
+    197: "GPS error or GPS signal lost",
+    198: "Motor malfunction",
+    199: "Battery critical",
+    200: "No GPS home position",
+    201: "Geofence violation",
+    202: "Internal clock error",
+    203: "External clock error",
+    204: "Required hardware component missing",
+    205: "Autopilot initialization failed",
+    206: "Autopilot communication failed",
+    207: "Drone crashed",
+    253: "Simulated critical error",
+    254: "Unspecified critical sensor failure",
+    255: "Unspecified critical error",
+}
+
+
 class FlockwaveErrorCode(IntEnum):
     """Error codes defined in the Flockwave protocol."""
 
@@ -103,3 +204,16 @@ class FlockwaveErrorCode(IntEnum):
     SIMULATED_CRITICAL_ERROR = 253
     CRITICAL_SENSOR_FAILURE = 254
     UNSPECIFIED_CRITICAL_ERROR = 255
+
+    @property
+    def abbreviation(self):
+        """Returns a short abbreviation of the error code that is more-or-less
+        human-readable, but requires less space on the screen than the full
+        description of the error.
+        """
+        return _error_code_to_abbreviation.get(self) or f"E{self}"
+
+    @property
+    def description(self):
+        """Returns a human-readable description of the error code."""
+        return _error_code_to_description.get(self) or f"Error {self}"

3c5685bdcf94f711a63caf29849efa59329f59ba
Tamas Nepusz <ntamas@gmail.com>
2020-06-08 16:27:50 +0200

    no need for an extended landing time after bugfix

--- src/flockwave/server/ext/cascade_demo.py
+++ src/flockwave/server/ext/cascade_demo.py
@@ -70,7 +70,7 @@ motoron=5
 takeoff=5 4 1 0
 waypoint=N{lat:.8f} E{lon:.8f} {agl:.2f} {velocity_xy:.2f} {velocity_z:.2f} 5 0
 # landing at station '{station}'
-land=4 1 10
+land=4 1 0
 motoroff=10
 """
 

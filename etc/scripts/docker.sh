6dad40fdeef4f879089e68e4c4b9968a4b20f9b1
Tamas Nepusz <ntamas@gmail.com>
2020-10-15 14:31:37 +0200

    build: tagging Docker image if we are exactly at a Git tag

--- etc/scripts/docker.sh
+++ etc/scripts/docker.sh
@@ -5,6 +5,8 @@
 SCRIPT_ROOT=`dirname $0`
 REPO_ROOT="${SCRIPT_ROOT}/../.."
 
+set -e
+
 cd ${REPO_ROOT}
 
 # Remove all requirements.txt files, we don't use them, only poetry
@@ -17,4 +19,11 @@ trap "rm -f requirements-main.txt" EXIT
 
 # Build the Docker image
 DOCKER_BUILDKIT=1 docker build -t docker.collmot.com/skybrush-server:latest -f etc/docker/amd64/Dockerfile .
+echo "Successfully built Docker image."
 
+# If we are at an exact tag, also tag the image
+GIT_TAG=`git describe --exact-match --tags 2>/dev/null || echo ""`
+if [ "x$GIT_TAG" != x ]; then
+    docker tag docker.collmot.com/skybrush-server:latest docker.collmot.com/skybrush-server:${GIT_TAG}
+    echo "Image tagged as $GIT_TAG."
+fi

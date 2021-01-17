bce06cd9e1af6497d3d3f81625dac8e859083390
Tamas Nepusz <ntamas@gmail.com>
2021-01-17 13:06:50 +0100

    feat: make it possible to generate an unobfuscated Windows installer

--- etc/scripts/build-nsis-installer.sh
+++ etc/scripts/build-nsis-installer.sh
@@ -5,10 +5,16 @@
 BUILD_DIR="./build/nsis"
 WHEEL_DIR="./build/wheels"
 OUTPUT_DIR="./dist/windows"
-OBFUSCATED_PACKAGES="aiocflib flockwave skybrush"
+OBFUSCATE=1
 PYTHON_VERSION=3.7.9
 
 ###############################################################################
+
+if [ "x$1" = "x--no-obfuscate" ]; then
+    echo "WARNING: creating unobfuscated build! Press ^C now if you don't want this."
+    sleep 5
+    OBFUSCATE=0
+fi
 
 set -e
 
@@ -55,6 +61,11 @@ cp etc/wheels/win32/*.whl "${WHEEL_DIR}"
 
 # TODO(ntamas): clean up unused MAVlink dialects somehow!
 
+INSTALLER_NAME="Skybrush Server Setup ${VERSION}.exe"
+if [ $OBFUSCATE -le 0 ]; then
+  INSTALLER_NAME="Unobfuscated ${INSTALLER_NAME}"
+fi
+
 # Create installer.cfg for pynsist
 # We temporarily copy skybrushd.py to the root because pynsist wants it to be
 # there. We could use an alternative path, but then that would be used in the
@@ -82,7 +93,7 @@ files=etc/blobs/win32/libusb-1.0.dll >\$INSTDIR\lib
     etc/deployment/nsis/skybrush.jsonc >\$INSTDIR
 
 [Build]
-installer_name=../../dist/windows/Skybrush Server Setup ${VERSION}.exe
+installer_name=../../dist/windows/${INSTALLER_NAME}
 EOF
 
 # Now clean the build dir and invoke pynsist, but don't run makensis just yet;
@@ -90,26 +101,28 @@ EOF
 rm -rf "${BUILD_DIR}"
 .venv/bin/python -m nsist installer.cfg --no-makensis
 
-# Install the _exact_ Python version that we are going to use with
-# PyArmor. PyArmor absolutely needs a matching Python version when doing
-# cross-platform builds
-pyenv install $PYTHON_VERSION --skip-existing
+if [ $OBFUSCATE -gt 0 ]; then
+  # Install the _exact_ Python version that we are going to use with
+  # PyArmor. PyArmor absolutely needs a matching Python version when doing
+  # cross-platform builds
+  pyenv install $PYTHON_VERSION --skip-existing
 
-# Create a virtualenv with the given Python version and install pyarmor in it
-PYARMOR_VENV_NAME=pyarmor-$PYTHON_VERSION
-PYARMOR_VENV=`pyenv virtualenv-prefix ${PYARMOR_VENV_NAME} 2>/dev/null || true`
-if [ "x${PYARMOR_VENV}" = x ]; then
-  pyenv virtualenv $PYTHON_VERSION $PYARMOR_VENV_NAME
-  PYARMOR_VENV=`pyenv virtualenv-prefix ${PYARMOR_VENV_NAME}`
-fi
-if [ "x${PYARMOR_VENV}" = x ]; then
-  echo "Could not create PyArmor virtualenv!"
-  exit 1
-fi
-${PYARMOR_VENV}/bin/pip install -U pyarmor
+  # Create a virtualenv with the given Python version and install pyarmor in it
+  PYARMOR_VENV_NAME=pyarmor-$PYTHON_VERSION
+  PYARMOR_VENV=`pyenv virtualenv-prefix ${PYARMOR_VENV_NAME} 2>/dev/null || true`
+  if [ "x${PYARMOR_VENV}" = x ]; then
+    pyenv virtualenv $PYTHON_VERSION $PYARMOR_VENV_NAME
+    PYARMOR_VENV=`pyenv virtualenv-prefix ${PYARMOR_VENV_NAME}`
+  fi
+  if [ "x${PYARMOR_VENV}" = x ]; then
+    echo "Could not create PyArmor virtualenv!"
+    exit 1
+  fi
+  ${PYARMOR_VENV}/bin/pip install -U pyarmor
 
-# Obfuscate the source
-PYARMOR_PLATFORM=darwin.x86_64.0 TARGET_PLATFORM=windows.x86.0 etc/scripts/apply-pyarmor-on-venv.sh ${PYARMOR_VENV}/bin/pyarmor "${BUILD_DIR}/pkgs" "${BUILD_DIR}/obf"
+  # Obfuscate the source
+  PYARMOR_PLATFORM=darwin.x86_64.0 TARGET_PLATFORM=windows.x86.0 etc/scripts/apply-pyarmor-on-venv.sh ${PYARMOR_VENV}/bin/pyarmor "${BUILD_DIR}/pkgs" "${BUILD_DIR}/obf"
+fi
 
 # Okay, call makensis now
 makensis "${BUILD_DIR}"/installer.nsi
@@ -119,4 +132,11 @@ rm skybrushd-win32.py
 
 echo ""
 echo "------------------------------------------------------------------------"
+
+echo ""
+echo "Installer created in dist/windows/${INSTALLER_NAME}"
+if [ $OBFUSCATE -le 0 ]; then
+  echo ""
+  echo "WARNING: THIS BUILD IS UNOBFUSCATED! DO NOT DISTRIBUTE!"
+fi
 

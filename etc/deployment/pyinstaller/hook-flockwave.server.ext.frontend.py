e768a7ae7a81e222771754a81446317cdbd84485
Tamas Nepusz <ntamas@gmail.com>
2021-01-19 00:21:05 +0100

    fix: added PyInstaller hook for flockwave.server.ext.frontend

---
+++ etc/deployment/pyinstaller/hook-flockwave.server.ext.frontend.py
@@ -0,0 +1,9 @@
+import flockwave.server.ext.frontend as frontend
+import os
+
+datas = [
+    (
+        os.path.join(os.path.dirname(frontend.__file__), "static"),
+        "flockwave/server/ext/frontend/static"
+    )
+]

100e409817f2d11393aa6bb2cfcf733cd74e6c42
Tamas Nepusz <ntamas@gmail.com>
2021-08-19 15:40:25 +0200

    feat: rc_udp channel ranges can now be overridden

--- src/flockwave/server/ext/rc_udp.py
+++ src/flockwave/server/ext/rc_udp.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 from functools import partial
 from logging import Logger
-from typing import Callable, Sequence, TYPE_CHECKING
+from typing import Any, Callable, Sequence, Optional, Tuple, TYPE_CHECKING
 
 from flockwave.connections import create_connection
 from flockwave.connections.socket import UDPListenerConnection
@@ -16,13 +16,17 @@ from flockwave.server.ports import get_port_number_for_service
 if TYPE_CHECKING:
     from flockwave.server.app import SkybrushServer
 
+#: Type specification for decoder functions that take a raw packet and return a
+#: list of RC channel values
+Decoder = Callable[[bytes], Sequence[int]]
+
 
 async def run(app: SkybrushServer, configuration, log):
     host = configuration.get("host", "127.0.0.1")
     port = configuration.get("port", get_port_number_for_service("rcin"))
     formatted_address = format_socket_address((host, port))
 
-    endianness = str(configuration.get("endianness", "big")).lower()
+    endianness = str(configuration.get("endianness", "little")).lower()
     bytes_per_channel = int(configuration.get("bytesPerChannel", 2))
     if endianness not in ("big", "little"):
         log.error("Endianness must be 'big' or 'little', disabling extension")
@@ -30,6 +34,9 @@ async def run(app: SkybrushServer, configuration, log):
     if bytes_per_channel != 1 and bytes_per_channel != 2:
         log.error("Bytes per RC channel must be 1 or 2, disabling extension")
         return
+
+    value_range = parse_range(configuration.get("range"))
+
     decoder = (
         decode_one_byte_per_channel
         if bytes_per_channel == 1
@@ -37,6 +44,9 @@ async def run(app: SkybrushServer, configuration, log):
         if endianness == "big"
         else decode_two_bytes_per_channel_little_endian
     )
+
+    if value_range is not None:
+        decoder = rescale(decoder, value_range)
 
     rc = app.import_api("rc")
     connection = create_connection(f"udp-listen://{host}:{port}")
@@ -52,6 +62,18 @@ async def run(app: SkybrushServer, configuration, log):
                 on_changed=rc.notify,
             ),
         )
+
+
+def parse_range(value: Any) -> Optional[Tuple[int, int]]:
+    """Parses the 'range' parameter from the configuration and returns the
+    lower and upper bound of each channel; the upper bound is exclusive.
+    """
+    if value is None:
+        return None
+    elif isinstance(value, (list, tuple)) and len(value) == 2:
+        return tuple(value)  # type: ignore
+    else:
+        raise ValueError("range must be None or an array of length 2")
 
 
 def decode_one_byte_per_channel(data: bytes) -> Sequence[int]:
@@ -81,11 +103,28 @@ def decode_two_bytes_per_channel_little_endian(data: bytes) -> Sequence[int]:
     return result
 
 
+def rescale(decoder: Decoder, bounds: Tuple[int, int]) -> Decoder:
+    min_value, max_value = bounds
+    ratio = 65535.0 / (max_value - min_value)
+
+    def scaled_decoder(data: bytes) -> Sequence[int]:
+        return [
+            0
+            if value < min_value
+            else 65535
+            if value > max_value
+            else int(round(value - min_value) * ratio)
+            for value in decoder(data)
+        ]
+
+    return scaled_decoder
+
+
 async def handle_udp_datagrams(
     connection: UDPListenerConnection,
     log: Logger,
     address: str,
-    decoder: Callable[[bytes], Sequence[int]],
+    decoder: Decoder,
     on_changed: Callable[[Sequence[int]], None],
 ) -> None:
     log.info(f"Listening for UDP RC input on {address}")
@@ -144,7 +183,7 @@ schema = {
             "type": "string",
             "title": "Endianness",
             "description": "Endianness of each incoming packet",
-            "default": "big",
+            "default": "little",
             "enum": ["big", "little"],
             "options": {
                 "enum_titles": [
@@ -152,6 +191,15 @@ schema = {
                     "Little endian (LSB first)",
                 ]
             },
+        },
+        "range": {
+            "title": "Override channel range",
+            "type": "array",
+            "format": "table",
+            "minItems": 2,
+            "maxItems": 2,
+            "items": {"type": "integer"},
+            "required": False,
         },
     }
 }

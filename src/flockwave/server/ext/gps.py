72fe16d6b0fef192c6cee5a41d62114f1fbe0a05
Tamas Nepusz <ntamas@gmail.com>
2021-12-16 16:15:21 +0100

    feat(gps): handle multiple GPS devices on one TCP listener

--- src/flockwave/server/ext/gps.py
+++ src/flockwave/server/ext/gps.py
@@ -13,7 +13,6 @@ from enum import Enum
 from functools import partial
 from json import loads
 from pynmea2 import parse as parse_nmea
-from trio import open_nursery
 from typing import Any, Dict, Optional, Tuple, TYPE_CHECKING
 
 from flockwave.gps.vectors import GPSCoordinate
@@ -32,6 +31,8 @@ from flockwave.server.errors import NotSupportedError
 from flockwave.server.model import ConnectionPurpose
 from flockwave.server.model.uav import PassiveUAVDriver
 from flockwave.spec.ids import make_valid_object_id
+
+from flockwave.server.utils.generic import overridden
 
 from .base import UAVExtensionBase
 
@@ -162,7 +163,6 @@ def parse_incoming_nmea_message(message: bytes) -> GPSMessage:
 
     if data.sentence_type == "RMC":
         result.update(
-            device="0",
             position=GPSCoordinate(lat=data.latitude, lon=data.longitude),
             heading=data.true_course,
         )
@@ -189,8 +189,10 @@ class GPSExtension(UAVExtensionBase):
     """
 
     _beacon_api: ExtensionAPIProxy
+    _connection_to_id: Dict[Connection, str]
     _device_to_beacon_id: Dict[str, str]
     _id_format: str
+    _main_connection: Optional[Connection]
 
     app: "SkybrushServer"
     driver: PassiveUAVDriver
@@ -199,38 +201,16 @@ class GPSExtension(UAVExtensionBase):
         """Constructor."""
         super().__init__()
         self._id_format = None  # type: ignore
-        self._device_to_beacon_id = {}
 
-    def _create_driver(self):
-        return PassiveUAVDriver()
+        self._connection_to_id = {}
+        self._device_to_beacon_id = {}
+        self._main_connection = None
 
     def configure(self, configuration):
         """Loads the extension."""
         self._id_format = configuration.get("id_format", "GPS:{0}")
 
-    async def handle_connection(
-        self, connection: Connection, format: MessageFormat
-    ) -> None:
-        """Worker task that handles a connection object.
-
-        If the connection is a listener connection, this function will wait for
-        incoming connections from clients and spawn additional tasks for them.
-        If the connection is a readable connection, it will forward everything
-        to `handle_gps_messages()`.
-
-        The connection is assumed to be connected or connecting by the time this
-        function is invoked.
-        """
-        await connection.wait_until_connected()
-
-        if isinstance(connection, ListenerConnection):
-            return await self._handle_listener(connection, format)
-        elif isinstance(connection, RWConnection):
-            return await self._handle_gps_messages(connection, format)
-        else:
-            raise RuntimeError("connection must be a listener or a readable connection")
-
-    async def _handle_gps_messages(
+    async def handle_gps_messages(
         self, connection: RWConnection[bytes, bytes], format: MessageFormat
     ) -> None:
         """Worker task that reads incoming messages from the given connection,
@@ -240,54 +220,83 @@ class GPSExtension(UAVExtensionBase):
         The connection is assumed to be open by the time this function is
         invoked.
         """
-        parser = format.create_parser()
-
         try:
             await connection.wait_until_connected()
-
-            async with ParserChannel(connection, parser) as channel:
-                async for message in channel:
-                    if "version" in message:
-                        # Ask gpsd to start streaming status data
-                        await connection.write(b'?WATCH={"enable":true,"json":true}\n')
-                    elif "device" in message:
-                        self._handle_single_gps_update(message)
-
+            if connection not in self._connection_to_id:
+                self._connection_to_id[connection] = self._derive_id_for_connection(
+                    connection
+                )
+            return await self._handle_gps_messages(connection, format.create_parser())
         except Exception as ex:
             if self.log:
                 self.log.exception(ex)
-
-    async def _handle_listener(
-        self,
-        connection: ListenerConnection[RWConnection[bytes, bytes]],
-        format: MessageFormat,
-    ) -> None:
-        address: Optional[str] = None
-
-        if hasattr(connection, "address"):
-            address = format_socket_address(connection.address)  # type: ignore
-
-        if self.log:
-            if address:
-                self.log.info(f"Listening for incoming GPS connections on {address}")
-            else:
-                self.log.info("Listening for incoming GPS connections")
-
-        try:
-            async with open_nursery() as nursery:
-                while True:
-                    next_connection: Connection = await connection.accept()
-                    nursery.start_soon(self.handle_connection, next_connection, format)
         finally:
-            if self.log:
-                if address:
-                    self.log.info(f"GPS connection listener on {address} closed")
-                else:
-                    self.log.info("GPS connection listener closed")
+            self._connection_to_id.pop(connection, None)
 
-    def _handle_single_gps_update(self, message: GPSMessage) -> None:
+    def _derive_id_for_connection(self, connection: RWConnection[bytes, bytes]):
+        """Derives a (preferably permanent) connection identififer for the
+        given connection, used as a prefix for GPS devices that send their
+        messages through this connection.
+        """
+        if hasattr(connection, "address"):
+            address = connection.address  # type: ignore
+            if isinstance(address, tuple) and len(address) >= 2:
+                return format_socket_address(address)
+        return ""  # Do not use a prefix for this connection
+
+    def _get_global_beacon_id(self, device_id: str) -> str:
+        """Returns the global beacon object ID (registered in the object registry
+        of the app) from the local device ID.
+        """
+        result = self._device_to_beacon_id.get(device_id)
+        if result is None:
+            result = make_valid_object_id(self._id_format.format(device_id))
+            self._device_to_beacon_id[device_id] = result
+        return result
+
+    def _get_local_device_id(self, message: GPSMessage, sender: Connection) -> str:
+        """Returns the local device ID from the GPS message that was sent and
+        the connection that the message was received from.
+        """
+        device_id_in_message = message.get("device")
+        sender_id = (
+            self._connection_to_id.get(sender)
+            if sender is not self._main_connection
+            else None
+        )
+
+        if device_id_in_message:
+            if sender_id:
+                # Connection has its own ID so add that as a prefix to the device ID
+                return f"{sender_id}:{device_id_in_message}"
+            else:
+                # Connection has no ID so just use the device ID
+                return device_id_in_message
+        else:
+            if sender_id:
+                # Connection has its own ID so use that
+                return sender_id
+            else:
+                # Connection has no ID either so just make up an ID
+                return "0"
+
+    async def _handle_gps_messages(
+        self, connection: RWConnection[bytes, bytes], parser: Parser[bytes, GPSMessage]
+    ) -> None:
+        async with ParserChannel(connection, parser) as channel:
+            async for message in channel:
+                if "version" in message:
+                    # Ask gpsd to start streaming status data
+                    await connection.write(b'?WATCH={"enable":true,"json":true}\n')
+                elif "position" in message:
+                    self._handle_single_gps_update(message, source=connection)
+
+    def _handle_single_gps_update(
+        self, message: GPSMessage, *, source: Connection
+    ) -> None:
         """Handles a single GPS status update message."""
-        beacon_id = self._get_beacon_id(message["device"])
+        device_id = self._get_local_device_id(message, source)
+        beacon_id = self._get_global_beacon_id(device_id)
 
         beacon: Optional[Beacon] = self._beacon_api.find_by_id(beacon_id)
         if not beacon:
@@ -298,35 +307,39 @@ class GPSExtension(UAVExtensionBase):
             position=message["position"], heading=message["heading"], active=True
         )
 
-    def _get_beacon_id(self, device_id: str) -> str:
-        """Returns the global beacon object ID (registered in the object registry
-        of the app) from the local device ID.
-        """
-        result = self._device_to_beacon_id.get(device_id)
-        if result is None:
-            result = make_valid_object_id(self._id_format.format(device_id))
-            self._device_to_beacon_id[device_id] = result
-        return result
+    async def run(self, app: "SkybrushServer", configuration, log):
+        self._beacon_api = app.import_api("beacon")
 
-    async def run(self, app: "SkybrushServer", configuration, logger):
         connection, format = create_gps_connection_and_format(
             connection=configuration.get("connection", "gpsd"),
         )
 
-        self._beacon_api = app.import_api("beacon")
+        address = None
+        if hasattr(connection, "address"):
+            address = format_socket_address(connection.address)  # type: ignore
+
+        is_listener = isinstance(connection, ListenerConnection)
+
+        if is_listener:
+            if address:
+                log.info(f"Listening for incoming GPS connections on {address}")
+            else:
+                log.info("Listening for incoming GPS connections")
 
         with ExitStack() as stack:
+            stack.enter_context(overridden(self, _main_connection=connection))
             stack.enter_context(
                 app.connection_registry.use(
                     connection,
                     "GPS",
-                    "GPS link",
+                    "GPS listener"
+                    if isinstance(connection, ListenerConnection)
+                    else "GPS link",
                     purpose=ConnectionPurpose.gps,  # type: ignore
                 )
             )
-
             await app.supervise(
-                connection, task=partial(self.handle_connection, format=format)
+                connection, task=partial(self.handle_gps_messages, format=format)
             )
 
 

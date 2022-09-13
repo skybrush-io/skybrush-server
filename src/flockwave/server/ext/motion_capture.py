dc867c889f4bbf4e9cada32c40f8d25869447228
Tamas Nepusz <ntamas@gmail.com>
2022-09-13 10:51:30 +0200

    feat(mocap): added MotionCaptureFrame.add_item()

--- src/flockwave/server/ext/motion_capture.py
+++ src/flockwave/server/ext/motion_capture.py
@@ -30,8 +30,8 @@ conventions; i.e., the order of items is ``(w, x, y, z)``.
 
 
 @dataclass(frozen=True)
-class MotionCaptureFrameEntry:
-    """An entry in a motion capture frame, containing a name, an optional 3D
+class MotionCaptureFrameItem:
+    """An item in a motion capture frame, containing a name, an optional 3D
     position and an optional attitude quaternion. The position may be omitted if
     tracking was lost for the rigid body. The attitude may also be omitted if
     it is not known. (Typically, the attitude is also omitted if the position
@@ -58,8 +58,26 @@ class MotionCaptureFrame:
     timestamp: float
     """The timestamp when the frame was obtained."""
 
-    items: List[MotionCaptureFrameEntry]
+    items: List[MotionCaptureFrameItem]
     """The rigid bodies in this frame, each represented by a name-pose pair."""
+
+    def add_item(
+        self,
+        name: str,
+        position: Optional[Position] = None,
+        attitude: Optional[Attitude] = None,
+    ) -> MotionCaptureFrameItem:
+        """Adds a new item to this frame and returns the newly added item.
+
+        Parameters:
+            name: the name of the rigid body
+            position: the position information; ``None`` if tracking was lost
+            attitude: the attitude quaternion; ``None`` if tracking was lost or
+                it is not tracked
+        """
+        item = MotionCaptureFrameItem(name=name, position=position, attitude=attitude)
+        self.items.append(item)
+        return item
 
 
 class FrameRateLimiter:

acfad10054f95c1d03cf0925347ad7360a166685
Gabor Vasarhelyi <vasarhelyi@collmot.com>
2023-01-16 09:38:18 +0100

    feat: MissionCommandBundle introduced

--- src/flockwave/server/model/mission.py
+++ src/flockwave/server/model/mission.py
@@ -3,13 +3,15 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from collections import Counter
+from dataclasses import dataclass, field
 from enum import Enum
 from typing import Any, Dict, List, Optional, Sequence, Tuple, TypedDict, Union
 
 from skybrush.geofence import get_geofence_configuration_from_show_specification
 
 from .geofence import GeofenceConfigurationRequest
+from .identifiers import default_id_generator
 
 
 __all__ = (
@@ -24,6 +26,7 @@ __all__ = (
     "PayloadAction",
     # mission commands
     "MissionCommand",
+    "MissionCommandBundle",
     "ChangeAltitudeMissionCommand",
     "ChangeHeadingMissionCommand",
     "ChangeSpeedMissionCommand",
@@ -34,9 +37,6 @@ __all__ = (
     "SetParameterMissionCommand",
     "TakeoffMissionCommand",
     "UpdateGeofenceMissionCommand",
-    # functions
-    "generate_mission_command_from_mission_item",
-    "generate_mission_commands_from_mission_items",
 )
 
 ################################################################################
@@ -111,6 +111,9 @@ class MissionItem(TypedDict):
     Skybrush Live.
     """
 
+    id: Optional[str]
+    """The (optional) unique identifier of the mission item."""
+
     type: str
     """The type of the mission item."""
 
@@ -121,8 +124,8 @@ class MissionItem(TypedDict):
 
 
 class MissionItemBundle(TypedDict):
-    """Representation of a collection of mission items submitted from Skybrush
-    Live.
+    """Representation of an ordered collection of mission items submitted from
+    Skybrush Live.
     """
 
     version: int
@@ -192,6 +195,49 @@ class PayloadAction(Enum):
 
 ################################################################################
 # MISSION COMMANDS
+
+
+def _generate_mission_command_from_mission_item(item: MissionItem) -> MissionCommand:
+    """Generate a mission command from a mission item.
+
+    Args:
+        item: the mission item to parse
+
+    Returns:
+        the parsed mission command
+
+    Raises:
+        RuntimeError on any parse error
+
+    """
+    _validate_mission_item(item)
+    type = MissionItemType(item["type"])
+    command: Optional[MissionCommand] = None
+
+    if type == MissionItemType.CHANGE_ALTITUDE:
+        command = ChangeAltitudeMissionCommand.from_json(item)
+    elif type == MissionItemType.CHANGE_HEADING:
+        command = ChangeHeadingMissionCommand.from_json(item)
+    elif type == MissionItemType.CHANGE_SPEED:
+        command = ChangeSpeedMissionCommand.from_json(item)
+    elif type == MissionItemType.GO_TO:
+        command = GoToMissionCommand.from_json(item)
+    elif type == MissionItemType.LAND:
+        command = LandMissionCommand.from_json(item)
+    elif type == MissionItemType.RETURN_TO_HOME:
+        command = ReturnToHomeMissionCommand.from_json(item)
+    elif type == MissionItemType.SET_PAYLOAD:
+        command = SetPayloadMissionCommand.from_json(item)
+    elif type == MissionItemType.SET_PARAMETER:
+        command = SetParameterMissionCommand.from_json(item)
+    elif type == MissionItemType.TAKEOFF:
+        command = TakeoffMissionCommand.from_json(item)
+    elif type == MissionItemType.UPDATE_GEOFENCE:
+        command = UpdateGeofenceMissionCommand.from_json(item)
+    else:
+        raise RuntimeError(f"Unhandled mission type: {type!r}")
+
+    return command
 
 
 def _get_altitude_from_parameters(params: Dict[str, Any]) -> Optional[Altitude]:
@@ -318,9 +364,14 @@ def _validate_mission_item(
     if not isinstance(item, dict):
         raise RuntimeError("mission item must be a dict")
 
+    # check optional id
+    id = item.get("id")
+    if id is not None and not isinstance(id, str):
+        raise RuntimeError("mission item ID must be a string")
+
+    # check required type
     if "type" not in item:
         raise RuntimeError("mission item must have a type")
-
     type = item["type"]
     try:
         type = MissionItemType(type)
@@ -329,6 +380,7 @@ def _validate_mission_item(
     if expected_type is not None and type != expected_type:
         raise RuntimeError(f"Mission type mismatch: {type!r}!={expected_type!r}")
 
+    # check optional parameters
     params = item.get("parameters")
     if params is not None and not isinstance(params, dict):
         raise RuntimeError("parameters must be a dict or None")
@@ -338,6 +390,13 @@ def _validate_mission_item(
 
 class MissionCommand(ABC):
     """Abstract superclass for mission commands."""
+
+    id: str
+    """The unique identifier of the mission command."""
+
+    def __post_init__(self):
+        if self.id is None:
+            self.id = default_id_generator()
 
     @classmethod
     @abstractmethod
@@ -359,6 +418,79 @@ class MissionCommand(ABC):
 
 
 @dataclass
+class MissionCommandBundle:
+    """Representation of an ordered collection of mission commands."""
+
+    version: int = 1
+    """The version number of the bundle; currently it is always 1."""
+
+    name: Optional[str] = None
+    """The name of the mission to upload to the drone."""
+
+    commands: List[MissionCommand] = field(default_factory=list)
+    """The list of mission commands in the bundle."""
+
+    def __post_init__(self):
+        self.check_ids()
+
+    @classmethod
+    def from_json(cls, bundle: MissionItemBundle):
+        """Parse a JSON-represented mission item bundle and convert it to a list
+        of mission commands.
+
+        Args:
+            bundle: the JSON representation of a mission item bundle
+
+        Returns:
+            parsed mission commands as a bundle
+
+        Raises:
+            RuntimeError on any parse error
+        """
+
+        if not isinstance(bundle, dict):
+            raise RuntimeError("mission items must be represented in a dict")
+
+        if bundle.get("version") != 1:
+            raise RuntimeError("only version 1 mission item bundles are supported")
+
+        items: Sequence[MissionItem] = bundle.get("items", ())
+        commands: List[MissionCommand] = []
+
+        for item in items:
+            commands.append(_generate_mission_command_from_mission_item(item))
+
+        return cls(version=1, name=bundle.get("name"), commands=commands)
+
+    def check_ids(self):
+        """Checks whether ids in the bundle are unique.
+
+        Raises:
+            RuntimeError if ids are not unique.
+        """
+        counter = Counter(command.id for command in self.commands)
+        if any(value > 1 for value in counter.values()):
+            raise RuntimeError("mission item ids are not unique")
+
+    @property
+    def json(self) -> MissionItemBundle:
+        """Returns JSON representation of mission commmands in mission item
+        bundle format.
+
+        Raises:
+            RuntimeError if ids of commands are not unique.
+
+        """
+        self.check_ids()
+
+        return {
+            "version": 1,
+            "name": self.name,
+            "items": [command.json for command in self.commands],
+        }
+
+
+@dataclass
 class ChangeAltitudeMissionCommand(MissionCommand):
     """Mission command that instructs the drone to change its altitude."""
 
@@ -373,17 +505,19 @@ class ChangeAltitudeMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.CHANGE_ALTITUDE, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
         alt = _get_altitude_from_parameters(params)
         if alt is None:
             raise RuntimeError("missing required parameter: 'alt'")
 
-        return cls(altitude=alt)
+        return cls(id=id, altitude=alt)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.CHANGE_ALTITUDE.value,
             "parameters": {
                 "alt": self.altitude.json,
@@ -410,15 +544,17 @@ class ChangeHeadingMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.CHANGE_HEADING, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
         heading = _get_heading_from_parameters(params)
 
-        return cls(heading=heading)
+        return cls(id=id, heading=heading)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.CHANGE_HEADING.value,
             "parameters": {
                 "heading": self.heading.json,
@@ -446,15 +582,17 @@ class ChangeSpeedMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.CHANGE_SPEED, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
         velocity_xy, velocity_z = _get_speed_from_parameters(params)
 
-        return cls(velocity_xy=velocity_xy, velocity_z=velocity_z)
+        return cls(id=id, velocity_xy=velocity_xy, velocity_z=velocity_z)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.CHANGE_SPEED.value,
             "parameters": {
                 "velocityXY": None
@@ -495,17 +633,19 @@ class GoToMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.GO_TO, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
         lat = _get_latitude_from_parameters(params)
         lon = _get_longitude_from_parameters(params)
         alt = _get_altitude_from_parameters(params)
 
-        return cls(latitude=lat, longitude=lon, altitude=alt)
+        return cls(id=id, latitude=lat, longitude=lon, altitude=alt)
 
     @property
     def json(self) -> MissionItem:
         retval = {
+            "id": self.id,
             "type": MissionItemType.GO_TO.value,
             "parameters": {
                 "lat": round(self.latitude, ndigits=7),
@@ -534,12 +674,13 @@ class LandMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.LAND, expect_params=False
         )
+        id = obj.get("id")
 
-        return cls()
+        return cls(id=id)
 
     @property
     def json(self) -> MissionItem:
-        return {"type": MissionItemType.LAND.value, "parameters": {}}
+        return {"id": self.id, "type": MissionItemType.LAND.value, "parameters": {}}
 
     @property
     def type(self) -> MissionItemType:
@@ -549,7 +690,7 @@ class LandMissionCommand(MissionCommand):
 @dataclass
 class ReturnToHomeMissionCommand(MissionCommand):
     """Mission command that instructs the drone to return to its home
-    coordinate.
+    coordinate horizontally.
     """
 
     velocity_xy: Optional[float] = None
@@ -563,12 +704,17 @@ class ReturnToHomeMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.RETURN_TO_HOME, expect_params=False
         )
+        id = obj.get("id")
 
-        return cls()
+        return cls(id=id)
 
     @property
     def json(self) -> MissionItem:
-        return {"type": MissionItemType.RETURN_TO_HOME.value, "parameters": {}}
+        return {
+            "id": self.id,
+            "type": MissionItemType.RETURN_TO_HOME.value,
+            "parameters": {},
+        }
 
     @property
     def type(self) -> MissionItemType:
@@ -592,16 +738,18 @@ class SetPayloadMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.SET_PAYLOAD, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
 
         name, action = _get_payload_action_from_parameters(params)
 
-        return cls(name=name, action=action)
+        return cls(id=id, name=name, action=action)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.SET_PAYLOAD.value,
             "parameters": {
                 "name": self.name,
@@ -631,6 +779,7 @@ class SetParameterMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.SET_PARAMETER, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
 
@@ -643,11 +792,12 @@ class SetParameterMissionCommand(MissionCommand):
                 "parameter value must be present as a string or a number"
             )
 
-        return cls(name=name, value=value)
+        return cls(id=id, name=name, value=value)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.SET_PARAMETER.value,
             "parameters": {"name": self.name, "value": self.value},
         }
@@ -672,17 +822,19 @@ class TakeoffMissionCommand(MissionCommand):
         _validate_mission_item(
             obj, expected_type=MissionItemType.TAKEOFF, expect_params=True
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
         alt = _get_altitude_from_parameters(params)
         if alt is None:
             raise RuntimeError("missing required parameter: 'alt'")
 
-        return cls(altitude=alt)
+        return cls(id=id, altitude=alt)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.TAKEOFF.value,
             "parameters": {
                 "alt": self.altitude.json,
@@ -708,6 +860,7 @@ class UpdateGeofenceMissionCommand(MissionCommand):
             expected_type=MissionItemType.UPDATE_GEOFENCE,
             expect_params=True,
         )
+        id = obj.get("id")
         params = obj["parameters"]
         assert params is not None
 
@@ -716,11 +869,12 @@ class UpdateGeofenceMissionCommand(MissionCommand):
         # FlatEarthToGPSCoordinateTransformation
         geofence = get_geofence_configuration_from_show_specification(params)
 
-        return cls(geofence=geofence)
+        return cls(id=id, geofence=geofence)
 
     @property
     def json(self) -> MissionItem:
         return {
+            "id": self.id,
             "type": MissionItemType.UPDATE_GEOFENCE.value,
             "parameters": {
                 "geofence": self.geofence.json,
@@ -731,83 +885,3 @@ class UpdateGeofenceMissionCommand(MissionCommand):
     @property
     def type(self) -> MissionItemType:
         return MissionItemType.UPDATE_GEOFENCE
-
-
-################################################################################
-# FUNCTIONS
-
-# TODO: this below might be more suited in the mission extension
-
-
-def generate_mission_command_from_mission_item(item: MissionItem) -> MissionCommand:
-    """Generate a mission command from a mission item.
-
-    Args:
-        item: the mission item to parse
-
-    Returns:
-        the parsed mission command
-
-    Raises:
-        RuntimeError on any parse error
-
-    """
-    _validate_mission_item(item)
-    type = MissionItemType(item["type"])
-    command: Optional[MissionCommand] = None
-
-    if type == MissionItemType.CHANGE_ALTITUDE:
-        command = ChangeAltitudeMissionCommand.from_json(item)
-    elif type == MissionItemType.CHANGE_HEADING:
-        command = ChangeHeadingMissionCommand.from_json(item)
-    elif type == MissionItemType.CHANGE_SPEED:
-        command = ChangeSpeedMissionCommand.from_json(item)
-    elif type == MissionItemType.GO_TO:
-        command = GoToMissionCommand.from_json(item)
-    elif type == MissionItemType.LAND:
-        command = LandMissionCommand.from_json(item)
-    elif type == MissionItemType.RETURN_TO_HOME:
-        command = ReturnToHomeMissionCommand.from_json(item)
-    elif type == MissionItemType.SET_PAYLOAD:
-        command = SetPayloadMissionCommand.from_json(item)
-    elif type == MissionItemType.SET_PARAMETER:
-        command = SetParameterMissionCommand.from_json(item)
-    elif type == MissionItemType.TAKEOFF:
-        command = TakeoffMissionCommand.from_json(item)
-    elif type == MissionItemType.UPDATE_GEOFENCE:
-        command = UpdateGeofenceMissionCommand.from_json(item)
-    else:
-        raise RuntimeError(f"Unhandled mission type: {type!r}")
-
-    return command
-
-
-def generate_mission_commands_from_mission_items(
-    bundle: MissionItemBundle,
-) -> List[MissionCommand]:
-    """Parse a mission item bundle and convert it to a list of mission commands.
-
-    Args:
-        bundle: the JSON representation of a mission item bundle
-
-    Returns:
-        list of parsed mission commands
-
-    Raises:
-        RuntimeError on any parse error
-
-    """
-
-    if not isinstance(bundle, dict):
-        raise RuntimeError("mission items must be represented in a dict")
-
-    if bundle.get("version") != 1:
-        raise RuntimeError("only version 1 mission item bundles are supported")
-
-    items: Sequence[MissionItem] = bundle.get("items", ())
-    commands: List[MissionCommand] = []
-
-    for item in items:
-        commands.append(generate_mission_command_from_mission_item(item))
-
-    return commands

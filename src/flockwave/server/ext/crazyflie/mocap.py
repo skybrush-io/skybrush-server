2291dd910818de477cb95a2fc660a19b416352d6
Tamas Nepusz <ntamas@gmail.com>
2023-04-07 12:47:21 +0200

    fix(cf): in mocap module, send pose information for those Crazyflies for which we have full pose info

--- src/flockwave/server/ext/crazyflie/mocap.py
+++ src/flockwave/server/ext/crazyflie/mocap.py
@@ -1,7 +1,12 @@
 from typing import List, Tuple, TYPE_CHECKING
 
 from aiocflib.crtp.crtpstack import CRTPPort
-from aiocflib.crazyflie.localization import Localization, LocalizationChannel
+from aiocflib.crazyflie.localization import (
+    GenericLocalizationCommand,
+    Localization,
+    LocalizationChannel,
+)
+from aiocflib.utils.quaternion import QuaternionXYZW
 
 from flockwave.server.utils import chunks
 
@@ -22,25 +27,34 @@ class CrazyflieMocapFrameHandler:
     _broadcaster: BroadcasterFunction
     _driver: "CrazyflieDriver"
 
-    def __init__(self, driver: "CrazyflieDriver", broadcaster: BroadcasterFunction):
+    send_pose: bool
+    """Whether to send full pose information if it is available."""
+
+    def __init__(
+        self,
+        driver: "CrazyflieDriver",
+        broadcaster: BroadcasterFunction,
+        *,
+        send_pose: bool = True
+    ):
         """Constructor."""
         self._broadcaster = broadcaster
         self._driver = driver
+        self.send_pose = bool(send_pose)
 
     def notify_frame(self, frame: "MotionCaptureFrame") -> None:
-        # TODO(ntamas): in theory, the signal that calls this function is
-        # rate-limited by the mocap extension, so this function is not called
-        # "too often" -- that's why there's no rate limiting here. Add rate
-        # limiting if this becomes a problem
-
-        # uav_ids = [item.name for item in frame.items]
-        # address_spaces = self._driver.sort_uav_ids_by_address_spaces(uav_ids)
+        # In theory, the signal that calls this function is rate-limited by the
+        # mocap extension, so this function is not called "too often" -- that's
+        # why there's no rate limiting here. Add rate limiting if this becomes
+        # a problem.
 
         # TODO(ntamas): broadcast only those that match the address space of
         # the broadcaster; the current solution does not work for multiple
         # Crazyradios in different address spaces yet
 
-        items: List[Tuple[int, Tuple[float, float, float]]] = []
+        positions: List[Tuple[int, Tuple[float, float, float]]] = []
+        poses: List[Tuple[int, Tuple[float, float, float], QuaternionXYZW]] = []
+
         for item in frame.items:
             # TODO(ntamas): currently we assume that the numeric ID of the
             # Crazyflie that we need to send in the localization packet is
@@ -52,15 +66,38 @@ class CrazyflieMocapFrameHandler:
                 numeric_id = int(item.name)
             except ValueError:
                 continue
-            if item.position is not None:
-                items.append((numeric_id, item.position))
+
+            if item.position is None:
+                # No position, we cannot do anything with this item
+                continue
+
+            if item.attitude is None or not self.send_pose:
+                # This item only has position information but no attitude
+                positions.append((numeric_id, item.position))
+            else:
+                # This item has both position and attitude info, but we need to
+                # convert the attitude to a QuaternionXYZW
+                w, x, y, z = item.attitude
+                poses.append((numeric_id, item.position, QuaternionXYZW(x, y, z, w)))
 
         # Crazyflie broadcast localization packets can accommodate coordinates
-        # for 4 drones
-        for chunk in chunks(items, 4):
+        # for 4 drones when we send only position information
+        for chunk in chunks(positions, 4):
             packet = Localization.encode_external_position_packed(chunk)
             self._broadcaster(
                 CRTPPort.LOCALIZATION,
                 LocalizationChannel.POSITION_PACKED,
+                packet,
+            )
+
+        # Crazyflie broadcast localization packets can accommodate coordinates
+        # for 2 drones when pose is also needed
+        for chunk in chunks(poses, 2):
+            packet = bytes(
+                [GenericLocalizationCommand.EXT_POSE_PACKED]
+            ) + Localization.encode_external_pose_packed(chunk)
+            self._broadcaster(
+                CRTPPort.LOCALIZATION,
+                LocalizationChannel.GENERIC,
                 packet,
             )

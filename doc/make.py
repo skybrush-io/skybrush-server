24da3fbba0481164e0af9720e4159b539ba940c8
Tamas Nepusz <ntamas@gmail.com>
2023-04-21 12:44:05 +0200

    doc: updated doc/make.py to create an archive automatically

--- doc/make.py
+++ doc/make.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
 from pathlib import Path
-from shutil import rmtree
+from shutil import make_archive, rmtree
 
 import pdoc
 import pdoc.render
@@ -19,8 +20,14 @@ if __name__ == "__main__":
 
     pdoc.pdoc(
         "flockwave",
-        "skybrush",
-        "!flockwave.protocols.mavlink",
-        "!flockwave.server.ext.socketio.vendor",
+        "!flockwave.app_framework",  # requires urwid
+        "!flockwave.protocols.mavlink",  # not needed, too much cruft
+        "!flockwave.server.ext.socketio.vendor",  # vendored code, not ours
         output_directory=out_dir,
     )
+
+    date = datetime.now().strftime("%Y%m%d")
+    archive_path = str(here / f"skybrush-server-docs-{date}")
+    make_archive(archive_path, "zip", out_dir)
+
+    print(f"Documentation archive created in {archive_path}.zip")

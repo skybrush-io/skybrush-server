2f215a0c4f97d94d776595751ac4ea4a30323d0d
Tamas Nepusz <ntamas@gmail.com>
2025-11-24 19:00:57 +0100

    refactor: remove unused get_address() and address=... from ChannelTypeDescriptor

---
+++ src/flockwave/server/ext/ssdp/types.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import Callable, ContextManager, Protocol, TypedDict, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .registry import UPnPServiceRegistry, URIOrCallableReturningURI
+
+__all__ = ("SSDPExtensionAPI",)
+
+
+class SSDPExtensionAPIDict(TypedDict):
+    register_service: Callable[[str, URIOrCallableReturningURI], None] | None
+    registry: UPnPServiceRegistry | None
+    unregister_service: Callable[[str], URIOrCallableReturningURI | None] | None
+    use_service: Callable[[str, URIOrCallableReturningURI], ContextManager[None]] | None
+
+
+class SSDPExtensionAPI(Protocol):
+    """Interface specification for the methods exposed by the ``ssdp``
+    extension.
+    """
+
+    register_service: Callable[[str, URIOrCallableReturningURI], None]
+    registry: UPnPServiceRegistry
+    unregister_service: Callable[[str], URIOrCallableReturningURI | None]
+    use_service: Callable[[str, URIOrCallableReturningURI], ContextManager[None]]
